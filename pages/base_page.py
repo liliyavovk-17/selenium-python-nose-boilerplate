6244d31b3e555f019aaa1c8696e5cfdd58c5c96e
Henry <henrywu1@users.noreply.github.com>
2017-03-07 13:59:38 -0500

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -15,14 +15,14 @@ class BasePageElement(object):
     def __set__(self, obj, value):
         """Sets the text to the value supplied."""
         driver = obj.driver
-        WebDriverWait(driver, 100).until(
+        WebDriverWait(driver, 10).until(
             lambda driver: driver.find_element(*self.locator))
         driver.find_element(*self.locator).send_keys(value)
 
     def __get__(self, obj, owner):
         """Gets the text of the specified object."""
         driver = obj.driver
-        WebDriverWait(driver, 100).until(
+        WebDriverWait(driver, 10).until(
             lambda driver: driver.find_element(*self.locator))
         element = driver.find_element(*self.locator)
         return element.get_attribute("value")
